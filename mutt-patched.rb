4adb1a8638c9ed5641c6cc0e87a1f2ce404310e8
Steve Clement <steve@localhost.lu>
2014-04-26 15:42:58 +0200

    - Updated to mutt-1.5.23 - Adapted to new brew patch format (checksum foo) - Kept sidebar patch alive

--- mutt-patched.rb
+++ mutt-patched.rb
@@ -2,8 +2,16 @@ require 'formula'
 
 class MuttPatched < Formula
   homepage 'http://www.mutt.org/'
-  url 'ftp://ftp.mutt.org/mutt/devel/mutt-1.5.22.tar.gz'
-  sha1 '728a114cb3a44df373dbf1292fc34dd8321057dc'
+  url 'ftp://ftp.mutt.org/mutt/mutt-1.5.23.tar.gz'
+  mirror 'http://fossies.org/linux/misc/mutt-1.5.23.tar.gz'
+  sha1 '8ac821d8b1e25504a31bf5fda9c08d93a4acc862'
+  revision 1
+
+  bottle do
+    sha1 "0b5b7346ec5450821d9416151426917aba099c5e" => :mavericks
+    sha1 "13e20a8a78933a64ba857ad3734ac45d421bd7e2" => :mountain_lion
+    sha1 "2772138cb1e6b63d0e0095790261fa6c2bad1068" => :lion
+  end
 
   head do
     url 'http://dev.mutt.org/hg/mutt#default', :using => :hg
@@ -16,40 +24,65 @@ class MuttPatched < Formula
     depends_on :automake
   end
 
+  unless Tab.for_name('signing-party').used_options.include? 'with-rename-pgpring'
+    conflicts_with 'signing-party',
+      :because => 'mutt installs a private copy of pgpring'
+  end
+
+  conflicts_with 'tin',
+    :because => 'both install mmdf.5 and mbox.5 man pages'
+
   option "with-debug", "Build with debug option enabled"
+  option "with-sidebar-patch", "Build with sidebar patch"
   option "with-trash-patch", "Apply trash folder patch"
-  option "with-slang", "Build against slang instead of ncurses"
+  option "with-s-lang", "Build against slang instead of ncurses"
   option "with-ignore-thread-patch", "Apply ignore-thread patch"
   option "with-pgp-verbose-mime-patch", "Apply PGP verbose mime patch"
-  option "with-pgp-multiple-crypt-hook-patch", "Apply PGP multiple-crypti-hook patch"
+  option "with-pgp-multiple-crypt-hook-patch", "Apply PGP multiple-crypt-hook patch"
+  option "with-pgp-combined-crypt-hook-patch", "Apply PGP combined-crypt-hook patch"
   option "with-confirm-attachment-patch", "Apply confirm attachment patch"
 
+  depends_on 'openssl'
   depends_on 'tokyo-cabinet'
   depends_on 's-lang' => :optional
+  depends_on 'gpgme' => :optional
 
-  def patches
-    urls = [
-      ['with-sidebar-patch', 'https://raw.github.com/nedos/mutt-sidebar-patch/master/mutt-sidebar.patch'],
-      ['with-trash-patch', 'http://patch-tracker.debian.org/patch/series/dl/mutt/1.5.21-6.4/features/trash-folder'],
-      # original source for this went missing, patch sourced from Arch at
-      # https://aur.archlinux.org/packages/mutt-ignore-thread/
-      ['with-ignore-thread-patch', 'https://gist.github.com/mistydemeo/5522742/raw/1439cc157ab673dc8061784829eea267cd736624/ignore-thread-1.5.21.patch'],
-      ['with-pgp-verbose-mime-patch',
-          'http://patch-tracker.debian.org/patch/series/dl/mutt/1.5.21-6.2/features-old/patch-1.5.4.vk.pgp_verbose_mime'],
-      ['with-pgp-multiple-crypt-hook-patch',
-          'http://localhost.lu/mutt/patches/patch-1.5.22.sc.multiple-crypt-hook.1'],
-      ['with-pgp-combined-crypt-hook-patch',
-          'http://localhost.lu/mutt/patches/patch-1.5.22.sc.crypt-combined.1'],
-      ['with-confirm-attachment-patch', 'https://gist.github.com/tlvince/5741641/raw/c926ca307dc97727c2bd88a84dcb0d7ac3bb4bf5/mutt-attach.patch'],
-    ]
+  patch do
+    url "http://patch-tracker.debian.org/patch/series/dl/mutt/1.5.21-6.2+deb7u1/features/trash-folder"
+    sha1 "6c8ce66021d89a063e67975a3730215c20cf2859"
+  end if build.with? "trash-patch"
 
-    p = []
-    urls.each do |u|
-      p << u[1] if build.include? u[0]
-    end
+  # original source for this went missing, patch sourced from Arch at
+  # https://aur.archlinux.org/packages/mutt-ignore-thread/
+  patch do
+    url "https://gist.github.com/mistydemeo/5522742/raw/1439cc157ab673dc8061784829eea267cd736624/ignore-thread-1.5.21.patch"
+    sha1 "dbcf5de46a559bca425028a18da0a63d34f722d3"
+  end if build.with? "ignore-thread-patch"
 
-    return p
-  end
+  patch do
+    url "http://patch-tracker.debian.org/patch/series/dl/mutt/1.5.21-6.2+deb7u1/features-old/patch-1.5.4.vk.pgp_verbose_mime"
+    sha1 "a436f967aa46663cfc9b8933a6499ca165ec0a21"
+  end if build.with? "pgp-verbose-mime-patch"
+
+  patch do
+    url "http://localhost.lu/mutt/patches/patch-1.5.22.sc.multiple-crypt-hook.1"
+    sha1 "697aae4e643f1e8f50c27b894ee6bfaab38d3119"
+  end if build.with? "pgp-multiple-crypt-hook-patch"
+
+  patch do
+    url "http://localhost.lu/mutt/patches/patch-1.5.22.sc.crypt-combined.1"
+    sha1 "2a12fe0a071e8cf7fe6f29336c6dadbccf95cdea"
+  end if build.with? "pgp-combined-crypt-hook-patch"
+
+  patch do
+    url "https://gist.github.com/tlvince/5741641/raw/c926ca307dc97727c2bd88a84dcb0d7ac3bb4bf5/mutt-attach.patch"
+    sha1 "94da52d50508d8951aa78ca4b073023414866be1"
+  end if build.with? "confirm-attachment-patch"
+
+  patch do
+    url "https://raw.github.com/nedos/mutt-sidebar-patch/master/mutt-sidebar.patch"
+    sha1 "1e151d4ff3ce83d635cf794acf0c781e1b748ff1"
+  end if build.with? "sidebar-patch"
 
   def install
     args = ["--disable-dependency-tracking",
