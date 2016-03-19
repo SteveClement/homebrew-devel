e1c5e1d38af3bf8c57b52cb8a9394391ab6c2226
steve <steve@localhost.lu>
2016-03-20 00:27:44 +0100

    updated checksums and removed pgp patches

--- mutt-patched.rb
+++ mutt-patched.rb
@@ -1,16 +1,17 @@
 require 'formula'
 
 class MuttPatched < Formula
+  desc "Mongrel of mail user agents (part elm, pine, mush, mh, etc.)"
   homepage 'http://www.mutt.org/'
-  url 'ftp://ftp.mutt.org/pub/mutt/mutt-1.5.24.tar.gz'
-  mirror 'http://fossies.org/linux/misc/mutt-1.5.24.tar.gz'
-  sha1 '38a2da5eb01ff83a90a2caee28fa2e95dbfe6898'
-  revision 1
+  url "https://bitbucket.org/mutt/mutt/downloads/mutt-1.5.24.tar.gz"
+  mirror "ftp://ftp.mutt.org/pub/mutt/mutt-1.5.24.tar.gz"
+  sha256 "a292ca765ed7b19db4ac495938a3ef808a16193b7d623d65562bb8feb2b42200"
 
   bottle do
-    sha1 "0b5b7346ec5450821d9416151426917aba099c5e" => :mavericks
-    sha1 "13e20a8a78933a64ba857ad3734ac45d421bd7e2" => :mountain_lion
-    sha1 "2772138cb1e6b63d0e0095790261fa6c2bad1068" => :lion
+    revision 2
+    sha256 "5bb0c9590b522bbcc38bfecaf0561810db2660792f472aa12a3b6c8f5e5b28d7" => :el_capitan
+    sha256 "8cad91b87b615984871b6bed35a029edcef006666bc7cf3b8f6b8b74d91c5b97" => :yosemite
+    sha256 "c57d868588eb947002902c90ee68af78298cbb09987e0150c1eea73f9e574cce" => :mavericks
   end
 
   head do
@@ -19,14 +20,11 @@ class MuttPatched < Formula
     resource 'html' do
       url 'http://dev.mutt.org/doc/manual.html', :using => :nounzip
     end
-
-    depends_on :autoconf
-    depends_on :automake
   end
 
-  unless Tab.for_name('signing-party').used_options.include? 'with-rename-pgpring'
-    conflicts_with 'signing-party',
-      :because => 'mutt installs a private copy of pgpring'
+  unless Tab.for_name("signing-party").with? "rename-pgpring"
+    conflicts_with "signing-party",
+      :because => "mutt installs a private copy of pgpring"
   end
 
   conflicts_with 'tin',
@@ -38,14 +36,17 @@ class MuttPatched < Formula
   option "with-s-lang", "Build against slang instead of ncurses"
   option "with-ignore-thread-patch", "Apply ignore-thread patch"
   option "with-pgp-verbose-mime-patch", "Apply PGP verbose mime patch"
-  option "with-pgp-multiple-crypt-hook-patch", "Apply PGP multiple-crypt-hook patch"
-  option "with-pgp-combined-crypt-hook-patch", "Apply PGP combined-crypt-hook patch"
   option "with-confirm-attachment-patch", "Apply confirm attachment patch"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
 
   depends_on 'openssl'
   depends_on 'tokyo-cabinet'
   depends_on 's-lang' => :optional
   depends_on 'gpgme' => :optional
+  depends_on "gettext" => :optional
+  depends_on "libidn" => :optional
 
   patch do
     url "http://localhost.lu/mutt/patches/trash-folder"
@@ -56,35 +57,27 @@ class MuttPatched < Formula
   # https://aur.archlinux.org/packages/mutt-ignore-thread/
   patch do
     url "https://gist.githubusercontent.com/mistydemeo/5522742/raw/1439cc157ab673dc8061784829eea267cd736624/ignore-thread-1.5.21.patch"
-    sha1 "dbcf5de46a559bca425028a18da0a63d34f722d3"
+    sha256 "7290e2a5ac12cbf89d615efa38c1ada3b454cb642ecaf520c26e47e7a1c926be"
   end if build.with? "ignore-thread-patch"
 
   patch do
-    url "http://patch-tracker.debian.org/patch/series/dl/mutt/1.5.21-6.2+deb7u1/features-old/patch-1.5.4.vk.pgp_verbose_mime"
-    sha1 "a436f967aa46663cfc9b8933a6499ca165ec0a21"
+    url "https://raw.githubusercontent.com/skn/homebrew/master/mutt/patches/patch-1.5.4.vk.pgp_verbose_mime"
+    sha256 "bb9108955aa8e1ddeb6a28ef60aa12713bc5c497e07f0544f556c6e6ad225280"
   end if build.with? "pgp-verbose-mime-patch"
 
   patch do
-    url "http://localhost.lu/mutt/patches/patch-1.5.23.sc.multiple-crypt-hook.1"
-    sha1 "697aae4e643f1e8f50c27b894ee6bfaab38d3119"
-  end if build.with? "pgp-multiple-crypt-hook-patch"
-
-  patch do
-    url "http://localhost.lu/mutt/patches/patch-1.5.23.sc.crypt-combined.1"
-    sha1 "2a12fe0a071e8cf7fe6f29336c6dadbccf95cdea"
-  end if build.with? "pgp-combined-crypt-hook-patch"
-
-  patch do
     url "https://gist.githubusercontent.com/tlvince/5741641/raw/c926ca307dc97727c2bd88a84dcb0d7ac3bb4bf5/mutt-attach.patch"
-    sha1 "94da52d50508d8951aa78ca4b073023414866be1"
+    sha256 "da2c9e54a5426019b84837faef18cc51e174108f07dc7ec15968ca732880cb14"
   end if build.with? "confirm-attachment-patch"
 
   patch do
     url "https://raw.githubusercontent.com/SteveClement/mutt-sidebar-patch/master/mutt-sidebar.patch"
-    sha1 "8bde2e1baf68a3239a248407c0cc3566f6d21174"
+    sha256 "ff92ef50811bc77ee1b7657aef6d2b5f48fae9e6d6dc0fd1dcd0296f983c21f4"
   end if build.with? "sidebar-patch"
 
   def install
+    user_admin = Etc.getgrnam("admin").mem.include?(ENV["USER"])
+
     args = ["--disable-dependency-tracking",
             "--disable-warnings",
             "--prefix=#{prefix}",
